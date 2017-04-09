0b7057df640e1e1bd44680b3475b64b2811c5678
rowella <alex+git@gormand.com.au>
2017-04-09 19:51:22 +1000

    SOMETHING AWESOME POST 2: Added in recognition of different types of fields

--- fuzzer.py
+++ fuzzer.py
@@ -1,4 +1,5 @@
-import requests, random, string, urllib, sys
+import requests, random, string, urllib, sys, copy
+from Argument import Argument
 
 #use the cookies
 cookies = {"PHPSESSID":"ao3pmpu53mi5nh2fgrkbdffjd6", "security_level": "0"}
@@ -14,15 +15,22 @@ valid_request = requests.get(url+"?"+get_args, cookies=cookies)
 
 print(valid_request.text)
 
+#Split it into each argument
+arg_list = get_args.split("&")
+
+arg_list = list(map(lambda x: Argument(x.split("=")[0], "=".join(x.split("=")[1:])), arg_list))
+
 while True:
     #Randomly change characters of the args and try it
-    new_args = list(get_args)
-    new_args[random.randint(0, len(new_args)-1)] = urllib.parse.quote(random.choice(string.punctuation))
+    new_args = copy.deepcopy(arg_list)
+    for arg in new_args:
+        arg.change()
 
-    new_args = "".join(new_args)
+    new_args = "&".join(list(map(str, new_args)))
     new_request = requests.get(url+"?"+new_args, cookies=cookies)
+
+    #print(url+"?"+new_args, file=sys.stderr)
 
     if ((not new_request.ok) or "error" in new_request.text.lower()):
         print("Error on: " + url+"?"+new_args)
         #quit()
-        #print(url+"?"+new_args, file=sys.stderr)
